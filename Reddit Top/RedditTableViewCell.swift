b7884b10ed7caaf8416ce70078319ce276d8c1f8
Yurii Koval <yurii.smith@gmail.com>
2020-10-22 15:52:43 +0300

    UI: Implemented basic UI

---
+++ Reddit Top/RedditTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RedditTableViewCell.swift
+//  Reddit Top
+//
+//  Created by Yurii Koval on 10/22/20.
+//  Copyright © 2020 Yurii Koval. All rights reserved.
+//
+
+import UIKit
+
+class RedditTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var thumbnailButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
